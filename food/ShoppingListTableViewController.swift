b1fd0b5d81765063b1b877afd4674aa4b7ad7607
Sabina Kim <sukang@umich.edu>
2018-07-19 18:35:11 -0400

    test2

--- food/ShoppingListTableViewController.swift
+++ food/ShoppingListTableViewController.swift
@@ -10,3 +10,15 @@ import UIKit
 
 var shoppingListArray = [String]()
 
+func init_shoppingListArray(){
+    shoppingListArray.append("3 Bananas")
+    shoppingListArray.append("2 cartons Milk")
+    shoppingListArray.append("8 Eggs")
+    shoppingListArray.append("3 v")
+    shoppingListArray.append("2 a")
+    shoppingListArray.append("8 s")
+    shoppingListArray.append("3 d")
+    shoppingListArray.append("2 f Milk")
+    shoppingListArray.append("8 g")
+    
+}
