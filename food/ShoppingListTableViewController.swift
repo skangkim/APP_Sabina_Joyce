3dac84b5d032465aa7156d97dcdd6d642e957e2b
Sabina Kim <sukang@umich.edu>
2018-07-19 18:35:47 -0400

    test3

--- food/ShoppingListTableViewController.swift
+++ food/ShoppingListTableViewController.swift
@@ -22,3 +22,92 @@ func init_shoppingListArray(){
     shoppingListArray.append("8 g")
     
 }
+
+class ShoppingListTableViewController: UITableViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        init_shoppingListArray()
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+        
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - Table view data source
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return shoppingListArray.count
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // Configure the cell...
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ShoppingListTableViewCell", for: indexPath) as? ShoppingListTableViewCell  else {
+            fatalError("The dequeued cell is not an instance of IngredientTableViewCell.")
+        }
+        
+        //fetches appropriate recipie for the data source layout
+        let recipie = shoppingListArray[indexPath.row]
+        // TODO: Find out how to fetch from dictionary
+        
+        let image = UIImage(named: "shoppinglistbutton.jpg")
+        cell.ring.image = image
+        
+        cell.nameLabel.text = recipie
+        
+        //on checkbox click
+        cell.onClick = { cell in
+            
+            let image = UIImage(named: "filledIn.jpg")
+            cell.filledInCircle.image = image
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) { // change 2 to desired number of seconds
+                // Your code with delay
+                shoppingListArray.remove(at: indexPath.row)
+                tableView.deleteRows(at: [indexPath], with: .fade)
+            }
+            
+            
+            //switch status
+            tableView.reloadData()
+        }
+        return cell
+    }
+    
+    /*
+     // Override to support conditional editing of the table view.
+     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
+    
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            shoppingListArray.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+            
+        }
+    }
+    
+}
