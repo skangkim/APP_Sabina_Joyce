93238a37b41657e83a768123aba8970dc9d5c827
joyylee <jlee9623@gmail.com>
2018-07-22 23:57:09 -0600

    Search tabby

---
+++ food/SearchCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  SearchCollectionViewCell.swift
+//  food
+//
+//  Created by J Lee on 7/22/18.
+//  Copyright © 2018 J Lee. All rights reserved.
+//
+
+import UIKit
+
+class SearchCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var recipeLabel: UILabel!
+    
+    override func layoutSubviews() {
+
+    }
+}
---
+++ food/SearchTableViewCell.swift
@@ -0,0 +1,34 @@
+//
+//  SearchTableViewCell.swift
+//  food
+//
+//  Created by J Lee on 7/22/18.
+//  Copyright © 2018 J Lee. All rights reserved.
+//
+
+import UIKit
+
+class SearchTableViewCell: UITableViewCell, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SearchCollectionViewCell", for: indexPath) as! SearchCollectionViewCell
+        cell.recipeLabel.text = "Hello!"
+        return cell
+    }
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
