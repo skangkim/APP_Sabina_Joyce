df4e0e76c8c82199c82eb76de382054af5939fe4
joyylee <jlee9623@gmail.com>
2018-07-23 22:12:18 -0600

    reorganized files

---
+++ food/Favorites Tab/FavoritesTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FavoritesTableViewCell.swift
+//  food
+//
+//  Created by J Lee on 7/23/18.
+//  Copyright © 2018 J Lee. All rights reserved.
+//
+
+import UIKit
+
+class FavoritesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
